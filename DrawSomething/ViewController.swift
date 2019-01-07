feb22114427c746cb5659a018922272315e63d94
Darrell Payne <darrellpayne@earthlink.net>
2019-01-07 14:09:21 -0800

    Basic line drawn on screen - no interactivity

--- DrawSomething/ViewController.swift
+++ DrawSomething/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Canvas: UIView {
+    override func draw(_ rect: CGRect) {
+        // Custom Drawing
+        super.draw(rect)
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        
+        // Setup lines to draw
+        
+        let startPoint = CGPoint(x: 0, y: 50)
+        let endPoint = CGPoint(x: 100, y: 100)
+        
+        context.move(to: startPoint)
+        context.addLine(to: endPoint)
+        
+        
+        context.strokePath()
+        
+    }
+    
+    // Capture screen touches
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
 
+class ViewController: UIViewController {
+    
+    let canvas = Canvas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(canvas)
+        canvas.frame = view.frame
+        canvas.backgroundColor = .white
     }
 
 
